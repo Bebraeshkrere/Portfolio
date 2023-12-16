7d523f7ccd3263fbde765f58f02ed69c8f23733a
Bebraeshkrere <144522850+Bebraeshkrere@users.noreply.github.com>
2023-12-16 18:28:12 +1000

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+let title = document.getElementsByClassName
+("header-title")
+console.log('Эл с классом header-title', title)
+
+let title2 = document.getElementsByTagName
+('h2')
+console.log('eses', title2)
+let switchBtn = document.getElementById('switch')
+console.log ('efeef', switchBtn)
+
+let navigation = document.querySelectorAll('.navigation-menu h3')
+let navitem = document.querySelector('navigation-menu h3')
+console.log(navitem)
+navitem.innerHTML = 'drgh'
