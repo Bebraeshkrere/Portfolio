a3d4ec147dc435c09f751903f6de47abe00eeb3d
Bebraeshkrere <144522850+Bebraeshkrere@users.noreply.github.com>
2024-01-27 18:30:17 +1000

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,24 @@
-let title = document.getElementsByClassName
-("header-title")
-console.log('Эл с классом header-title', title)
+let work = document.getElementById('work')
+let education = document.getElementById('education')
+let list = document.querySelector('.page-about__profile-list');
+console.log(work, education)
 
-let title2 = document.getElementsByTagName
-('h2')
-console.log('eses', title2)
-let switchBtn = document.getElementById('switch')
-console.log ('efeef', switchBtn)
+work.onclick = Click() {
 
-let navigation = document.querySelectorAll('.navigation-menu h3')
-let navitem = document.querySelector('navigation-menu h3')
-console.log(navitem)
-navitem.innerHTML = 'drgh'
+}
+work.onclick = function () {
+    console.log('Нажали на Work');
+    list.innerHTML()
+    list.insertAdjacentHTML("afterbegin", `<article class="profile-item">
+    <h5>computer engineer</h5>
+    <div class="profile-item__bottom">
+      <h5>Peru - University</h5>
+      <div class="profile-item__date"></div>
+        <img src="./assets/img/calenadar.svg" alt="">
+        <h6>2009 - 2014</h6>
+    </div>
+  </article>`);
+};
+   
+education.onclick = Click
+    console.log('Нажали на education')
